57853fa0a632d694f404f718e1e1ea97417f94a6
xxdl <xxdl@xxdl.top>
2025-08-15 21:04:50 +0800

    添加nuxt ui 和auth-utils

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- <div class="flex w-80 h-20 bg-red-200"> -->
+    <UButton>按钮</UButton>
+    <UBanner title="This is a banner with an important message." />
+     <UColorModeSwitch >前后</UColorModeSwitch>
+  <!-- </div> -->
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+    modules: ['nuxt-auth-utils','@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  ui: {
+    fonts: false
+  }
+
 })
